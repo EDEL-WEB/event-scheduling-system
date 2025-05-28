d44565f5ef06d93bbd9dbd223ea9e03cfa7e771d
edel <edel.omondi@student.moringaschool.com>
2025-05-28 09:29:22 +0300

    created events and bookings

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -15,13 +15,13 @@ users = [User(username=faker.name(), email=faker.email()) for _ in range(5)]
 session.add_all(users)
 session.commit()
 
-# Create events
+
 events = [Event(title=faker.catch_phrase(), description=faker.text(), event_date=faker.future_date()) for _ in range(5)]
 
 session.add_all(events)
 session.commit()
 
-# Create bookings
+
 bookings = [
     Booking(user_id=random.choice(users).id, event_id=random.choice(events).id)
     for _ in range(10)
